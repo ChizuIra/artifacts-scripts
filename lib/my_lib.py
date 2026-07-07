a05dbb9a047f53926a7fc70644c4b2014c2223f5
ChizuIra <kishidq@gmail.com>
2026-07-07 17:54:34 +0200

    ajout du stockage automatique dans la bank

--- lib/my_lib.py
+++ lib/my_lib.py
@@ -1,4 +1,4 @@
-TOKEN = "your token here" 
+TOKEN = "token here" 
 
 from time import sleep
 import requests
@@ -9,9 +9,34 @@ chiken_farm = { "x": 0, "y": 1 }
 y_slime = { "x": 1,"y":-2 }
 cooking = { "x": 1, "y": 1 }
 weapon_crafting = { "x": 2, "y": 1 }
+bank = { "x": 4, "y": 1 }
 
-# Actions function
-def move(character,body):
+
+
+# GET functions
+def get_character(character):
+    url = f"https://api.artifactsmmo.com/characters/{character}"
+    headers = {
+        "Accept": "application/json",
+        "Authorization": f"Bearer {TOKEN}"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        if "error" in data:
+            raise Exception(data["error"]["message"])
+
+        print("Successfully fetched character.")
+        print(data["data"]["cooldown"]) 
+        sleep(data["data"]["cooldown"])
+    except Exception as e:
+        print(f"❌ {e}")
+    print("---")
+    return data
+
+# actions
+def move(character,destination):
     # API endpoint for the move action
     url = f"https://api.artifactsmmo.com/my/{character}/action/move"
     headers = {
@@ -20,22 +45,21 @@ def move(character,body):
         "Authorization": f"Bearer {TOKEN}"
     }
 
-    try:
-        response = requests.post(url, headers=headers, json=body)
-        data = response.json()
+    response = requests.post(url, headers=headers, json=destination)
+    data = response.json()
 
-        if "error" in data:
-            raise Exception(data["error"]["message"])
+    if "error" in data:
+        print(f"❌ {data["error"]["message"]}")
+        return data 
 
-        destination = data["data"]["destination"]
-        cooldown = data["data"]["cooldown"]
+    destination = data["data"]["destination"]
+    cooldown = data["data"]["cooldown"]
 
-        print(f"✅ Moved to ({destination['x']}, {destination['y']}) on {destination['name']}")
-        print(f"⏳ Cooldown started: {cooldown['total_seconds']} seconds")
-    except Exception as e:
-        print(f"❌ {e}")
-    print("---")
+    print(f"✅ Moved to ({destination['x']}, {destination['y']}) on {destination['name']}")
+    print(f"⏳ Cooldown started: {cooldown['total_seconds']} seconds")
     sleep(cooldown['total_seconds'])
+    print("---")
+    return data
 
 def fight(character):
     # API endpoint to start a fight against the monster on the current tile
@@ -46,27 +70,26 @@ def fight(character):
         "Authorization": f"Bearer {TOKEN}"
     }
     
-    try:
-        response = requests.post(url, headers=headers)
-        data = response.json()
+    response = requests.post(url, headers=headers)
+    data = response.json()
 
-        if "error" in data:
-            raise Exception(data["error"]["message"])
+    if "error" in data:
+        print(f"❌ {data["error"]["message"]}")
+        return data 
 
-        fight = data["data"]["fight"]
-        fight_stats = fight["characters"][0]
-        cooldown = data["data"]["cooldown"]
+    fight = data["data"]["fight"]
+    fight_stats = fight["characters"][0]
+    cooldown = data["data"]["cooldown"]
 
-        print("🏆 Fight won!" if fight["result"] == "win" else "💀 Fight lost!")
-        print(f"⚔️  XP gained: {fight_stats['xp']} | HP remaining: {fight_stats['final_hp']}")
-        if len(fight_stats["drops"]) > 0:
-            drops_str = ", ".join([f"{d['quantity']}x {d['code']}" for d in fight_stats["drops"]])
-            print(f"🎁 Loot dropped: {drops_str}")
-        print(f"⏳ Cooldown started: {cooldown['total_seconds']} seconds")
-    except Exception as e:
-        print(f"❌ {e}")
-    print("---")
+    print("🏆 Fight won!" if fight["result"] == "win" else "💀 Fight lost!")
+    print(f"⚔️  XP gained: {fight_stats['xp']} | HP remaining: {fight_stats['final_hp']}")
+    if len(fight_stats["drops"]) > 0:
+        drops_str = ", ".join([f"{d['quantity']}x {d['code']}" for d in fight_stats["drops"]])
+        print(f"🎁 Loot dropped: {drops_str}")
+    print(f"⏳ Cooldown started: {cooldown['total_seconds']} seconds")
     sleep(cooldown['total_seconds'])
+    print("---")
+    return data
 
 def rest(character):
     # API endpoint to make your character rest and recover HP
@@ -90,9 +113,50 @@ def rest(character):
         print(f"🛏️   Rested and restored {hp_restored} HP.")
         print(f"❤️  Current HP: {character['hp']}/{character['max_hp']}")
         print(f"⏳ Cooldown started: {cooldown['total_seconds']} seconds")
+        sleep(cooldown['total_seconds'])
     except Exception as e:
         print(f"❌ {e}")
     print("---")
+
+def deposit_bank(character,code_item,quantity):
+    url = f"https://api.artifactsmmo.com/my/{character}/action/bank/deposit/item"
+    payload = [
+        {
+            "code": f"{code_item}",
+            "quantity": quantity
+        }
+    ]
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {TOKEN}"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+    
+    if "error" in data:
+        print(f"❌ {data["error"]["message"]}")
+        return data 
+
+    cooldown = data["data"]["cooldown"]
+    
+    print(f"Deposit x{quantity} {code_item} in bank")
+    print(f"⏳ Cooldown started: {cooldown['total_seconds']} seconds")
+    
     sleep(cooldown['total_seconds'])
+    print("---")
+    return data
 
 
+# send the sauce deep into the bank~
+def cuming_inventory_into_bank(character):
+    data = move(character,bank)
+    i = 0
+    for x in data["data"]["character"]["inventory"]:
+        slot_x = data["data"]["character"]["inventory"][i]
+        if slot_x["quantity"] != 0:
+            #print(f"deposit x{slot_x["quantity"]} {slot_x["code"]}")
+            deposit_bank(character,slot_x["code"],slot_x["quantity"])    
+        i = i + 1
+            
--- script.py
+++ script.py
@@ -1,10 +1,18 @@
 from lib.my_lib import *
 
 character = "char1"
+farming_zone = chiken_farm
 
 # script start here
-move(character,chiken_farm)
-move(character,y_slime)
+move(character,farming_zone)
 while True:
     rest(character)
-    fight(character)
+    response = fight(character)
+    try:
+        if response["error"]["code"] == 497:
+            cuming_inventory_into_bank(character)
+            move(character,farming_zone)
+    except:
+        continue
+
+
